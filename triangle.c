efddcd9b859e236ca1b0c7f9e7e456168dd44892
dimdiablotin <dimdiablotin@yahoo.fr>
2018-07-09 23:08:37 +0200

    Ajout de la boucle des lignes dans triangle.c

---
+++ triangle.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int n; //nombre de lignes
+
+    printf("Nombre de lignes : ");
+    scanf("%d",&n);
+
+    for(i=0; i<=n; i++) //affichage des lignes
+    {
+        printf("\n");
+    }
+    return 0;
+}
