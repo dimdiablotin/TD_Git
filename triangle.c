7a5b09329d28d3c93535c4cd2a5cd59d8e57d9fe
dimdiablotin <dimdiablotin@yahoo.fr>
2018-07-09 23:12:03 +0200

    Ajout de la boucle des '*' dans triangle.c

--- triangle.c
+++ triangle.c
@@ -4,18 +4,20 @@
 int main()
 {
     int n; //nombre de lignes
-	int i; // compteur des lignes
-	int j;  //compteur des caractères
+    int i; // compteur des lignes
+    int j;  //compteur des caractères
 
     printf("Nombre de lignes : ");
     scanf("%d",&n);
 
     for(i=0; i<=n; i++) //affichage des lignes
     {
-		for (j=0; j<n-i; j++) //affichage des espaces
+        for (j=0; j<n-i; j++) //affichage des espaces
             printf(" ");
 
-			
+        for (j=0; j<2*i-1; j++) // affichage des '*'
+            printf("*");
+
         printf("\n");
     }
     return 0;
