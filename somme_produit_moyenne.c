7114bbd75aef79368bc31f09676012494bb4cf88
dimdiablotin <dimdiablotin@yahoo.fr>
2018-07-09 23:18:01 +0200

    Ajout de la saisie de l'utilisateur dans somme_produit_moyenn.c

---
+++ somme_produit_moyenne.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int nombreEntier,i=0,entier;
+    int produit=1, somme=0;
+    float moyenne=0;
+
+    printf("Combien d'entiers ? ");
+    scanf("%d",&nombreEntier);
+
+}
